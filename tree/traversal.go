1167c59bcc52d9a88bab706dd99a4db7d058ffc8
shani pathak <shani.pathak98@gmail.com>
2022-07-08 14:19:43 +0530

    Added tree level order traversal

--- tree/traversal.go
+++ tree/traversal.go
@@ -1,6 +1,9 @@
 package tree
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/shani1998/data-structures/queue"
+)
 
 func PreOrderTraversal(root *Node) {
 	if root == nil {
@@ -27,4 +30,59 @@ func InOrderTraversal(root *Node) {
 	InOrderTraversal(root.left)
 	fmt.Printf("%v ", root.val)
 	InOrderTraversal(root.right)
+}
+
+func LevelOrderTraversal(root *Node) {
+	if root == nil {
+		return
+	}
+	// implement BFS algorithm using Queue
+	queue := queue.NewQueue(100)
+	queue.Push(root)
+
+	for queue.Length() > 0 {
+		item, _ := queue.Pop()
+		fmt.Printf("%v-->", item.(*Node).val)
+		if item.(*Node).left != nil {
+			queue.Push(item.(*Node).left)
+		}
+		if item.(*Node).right != nil {
+			queue.Push(item.(*Node).right)
+		}
+	}
+
+}
+
+func GetLevelOrderTraversal(root *Node) [][]any {
+	result := make([][]any, 0)
+	if root == nil {
+		return result
+	}
+
+	queue := queue.NewQueue(100)
+	queue.Push(root)
+
+	for queue.Length() > 0 {
+		Qsize := queue.Length()
+		levels := make([]any, 0)
+
+		// pop all elements of current level
+		// and add its children to the queue
+		for i := 0; i < Qsize; i++ {
+			item, _ := queue.Pop()
+			levels = append(levels, item)
+			if item.(*Node).left != nil {
+				queue.Push(item.(*Node).left)
+			}
+			if item.(*Node).right != nil {
+				queue.Push(item.(*Node).right)
+			}
+		}
+
+		// app levels to the result list
+		result = append(result, levels)
+	}
+
+	return result
+
 }
